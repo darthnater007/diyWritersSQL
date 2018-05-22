c909fd11b56a95a0b9b819939d550bf22f180189
darthnater007 <nsperk@gmail.com>
2018-05-22 17:09:31 -0400

    added a foreign key to the piece table that I forgot to add earlier, also added another SQL script to test inserts for user, figuring out insert for piece

---
+++ diyWritersTestInserts.sql
@@ -0,0 +1,2 @@
+INSERT INTO diyWriters.User (UserName, Password, FirstName, LastName, Bio, Email, Phone)
+VALUES ("Test1", "test", "Test", "Testerson", "testing bio", "test@test.test", "xxx-xxx-xxxx");
